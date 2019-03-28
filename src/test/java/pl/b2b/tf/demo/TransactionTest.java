dcb645ef7168e505f7bf570115c943b4262f1b4c
dorotaglazek <dorota.glazek@gmail.com>
2019-03-28 22:30:38 +0100

    zakomentowany test

--- src/test/java/pl/b2b/tf/demo/TransactionTest.java
+++ src/test/java/pl/b2b/tf/demo/TransactionTest.java
@@ -6,14 +6,14 @@ import pl.b2b.net.tf.demo.testfactory.TestFactorySteps;
 
 public class TransactionTest {
 
-    @Test
-    public void transactionTest1() throws Throwable {
-        BaseTF.setUp("FF");
-        TestFactorySteps testFactorySteps = new TestFactorySteps();
-        testFactorySteps.setUp();
-        testFactorySteps.yourJourneyTest("Europa","Wypoczynek","20190405","20190411",
-                "1","0","31,36","53,90","91,14");
-        BaseTF.tearDown();
-    }
+//    @Test
+//    public void transactionTest1() throws Throwable {
+//        BaseTF.setUp("FF");
+//        TestFactorySteps testFactorySteps = new TestFactorySteps();
+//        testFactorySteps.setUp();
+//        testFactorySteps.yourJourneyTest("Europa","Wypoczynek","20190405","20190411",
+//                "1","0","31,36","53,90","91,14");
+//        BaseTF.tearDown();
+//    }
 
 }
