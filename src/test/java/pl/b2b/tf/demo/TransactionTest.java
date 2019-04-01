da87b90f121fc2e9d2c9e99c5b149f9a6cee3ecd
dorotaglazek <dorota.glazek@gmail.com>
2019-04-01 09:18:28 +0200

    koment

--- src/test/java/pl/b2b/tf/demo/TransactionTest.java
+++ src/test/java/pl/b2b/tf/demo/TransactionTest.java
@@ -21,31 +21,31 @@ public class TransactionTest {
 //    }
 
 
-    @DataProvider(name = "TestData")
-    public Object[][] getData() throws IOException {
-        DataLoader dataLoader = new DataLoader();
-        Object[][] tab = dataLoader.read(GlobalDefinitions.EXCEL_FILE_PATH);
-        return tab;
-    }
-
-    @BeforeClass
-    public void transactionTestDataProviderStart() throws Throwable {
-        BaseTF.setUp("FF");
-    }
-    @Test(dataProvider = "TestData")
-    @Parameters({"direction", "destination", "dateOfDeparture", "dateOfReturn", "numberOfAdults", "numberOfChildren", "standardProtection", "fullComfort", "prestigiousJourney"})
-    public void transactionTestDataProvider(String direction, String destination, String dateOfDeparture, String dateOfReturn, String numberOfAdults,
-                                            String numberOfChildren, String standardProtection, String fullComfort, String prestigiousJourney) throws Throwable {
-
-        TestFactorySteps testFactorySteps = new TestFactorySteps();
-        testFactorySteps.setUp();
-        testFactorySteps.yourJourneyTestDP(direction,destination,dateOfDeparture,dateOfReturn,
-                numberOfAdults,numberOfChildren,standardProtection,fullComfort,prestigiousJourney);
-
-    }
-
-        @AfterClass
-    public void transactionTestDataProviderEnd() {
-            BaseTF.tearDown();
-        }
+//    @DataProvider(name = "TestData")
+//    public Object[][] getData() throws IOException {
+//        DataLoader dataLoader = new DataLoader();
+//        Object[][] tab = dataLoader.read(GlobalDefinitions.EXCEL_FILE_PATH);
+//        return tab;
+//    }
+//
+//    @BeforeClass
+//    public void transactionTestDataProviderStart() throws Throwable {
+//        BaseTF.setUp("FF");
+//    }
+//    @Test(dataProvider = "TestData")
+//    @Parameters({"direction", "destination", "dateOfDeparture", "dateOfReturn", "numberOfAdults", "numberOfChildren", "standardProtection", "fullComfort", "prestigiousJourney"})
+//    public void transactionTestDataProvider(String direction, String destination, String dateOfDeparture, String dateOfReturn, String numberOfAdults,
+//                                            String numberOfChildren, String standardProtection, String fullComfort, String prestigiousJourney) throws Throwable {
+//
+//        TestFactorySteps testFactorySteps = new TestFactorySteps();
+//        testFactorySteps.setUp();
+//        testFactorySteps.yourJourneyTestDP(direction,destination,dateOfDeparture,dateOfReturn,
+//                numberOfAdults,numberOfChildren,standardProtection,fullComfort,prestigiousJourney);
+//
+//    }
+//
+//        @AfterClass
+//    public void transactionTestDataProviderEnd() {
+//            BaseTF.tearDown();
+//        }
 }
